7d7a3a4800409ce9a67c3cbe365b65d448fc5897
Basil Elsaid <basil_elsaid@outlook.com>
2026-07-04 12:49:13 +0200

    Update app.routes.ts

--- frontend/app-tesi/src/app/app.routes.ts
+++ frontend/app-tesi/src/app/app.routes.ts
@@ -1,17 +1,10 @@
 import { Routes } from '@angular/router';
 import { roleGuard } from './core/guards/role.guard';
-import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: HomeComponent,
     children: [
-      {
-        path: '',
-        loadComponent: () =>
-          import('./pages/home/home.component').then((m) => m.HomeComponent),
-      },
       {
         path: '',
         loadComponent: () =>
